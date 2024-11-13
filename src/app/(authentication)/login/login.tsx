cfac744bfbf0d601b46eb6e66ac8e31324ec4fde
Alex Song <songhongli@gmail.com>
2024-11-12 17:12:36 -0800

    integrate the Dynamic login

--- src/app/(authentication)/login/login.tsx
+++ src/app/(authentication)/login/login.tsx
@@ -12,12 +12,76 @@ import InputGroupText from 'react-bootstrap/InputGroupText'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import useDictionary from '@/locales/dictionary-hook'
-
+import dynamic from 'next/dynamic';
+import { SolanaWalletConnectors } from "@dynamic-labs/solana";
+import { http } from "viem";
+import { mainnet } from "viem/chains";
+import { createConfig, WagmiProvider } from "wagmi";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 export default function Login({ callbackUrl }: { callbackUrl: string }) {
+  const DynamicWidget = dynamic(
+  () => import('@dynamic-labs/sdk-react-core').then((mod) => mod.DynamicWidget),
+  { ssr: false }
+  );
+  const DynamicContextProvider = dynamic(
+  () =>
+    import("@dynamic-labs/sdk-react-core").then(
+      (mod) => mod.DynamicContextProvider,
+    ),
+  { ssr: false },
+);
+
+const DynamicWagmiConnector = dynamic(
+  () =>
+    import("@dynamic-labs/wagmi-connector").then(
+      (mod) => mod.DynamicWagmiConnector,
+    ),
+  { ssr: false },
+  );
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) {
+      browserQueryClient = makeQueryClient();
+    }
+    return browserQueryClient;
+  }
+}
+  const config = createConfig({
+  chains: [mainnet],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+const cssOverrides = `
+.button--padding-large {
+  padding: 10px 20px !important;
+}
+`;
   const [submitting, setSubmitting] = useState(false)
   const [error, setError] = useState('')
   const router = useRouter()
   const dict = useDictionary()
+  const queryClient = getQueryClient();
 
   const login = async (formData: FormData) => {
     setSubmitting(true)
@@ -58,6 +122,7 @@ export default function Login({ callbackUrl }: { callbackUrl: string }) {
       setSubmitting(false)
     }
   }
+    const dynamicEnvironmentId = process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID;
 
   return (
     <>
@@ -69,6 +134,26 @@ export default function Login({ callbackUrl }: { callbackUrl: string }) {
       >
         {error}
       </Alert>
+      <DynamicContextProvider
+          settings={{
+            cssOverrides: cssOverrides,
+            environmentId: dynamicEnvironmentId || '',
+            walletConnectors: [
+              EthereumWalletConnectors,
+              SolanaWalletConnectors,
+            ],
+          }}
+        >
+          <WagmiProvider config={config}><QueryClientProvider client={queryClient}>
+              <DynamicWagmiConnector>
+          <DynamicWidget
+            buttonClassName="dynamic-inner-button custom-login-button"
+            innerButtonComponent={<span>Log in</span>}
+          />         
+            </DynamicWagmiConnector>            </QueryClientProvider>
+
+          </WagmiProvider>
+      </DynamicContextProvider>              
       <Form action={login}>
         <InputGroup className="mb-3">
           <InputGroupText>
